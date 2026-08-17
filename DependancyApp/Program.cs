f64acb6ad6599c67f660ed320dbf3bfdc5c850ec
Judah Walsh <judahw58@gmail.com>
2026-08-17 12:58:32 +1000

    Added dependancy app

---
+++ DependancyApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+
+static string promptFor(string prompt)
+{
+	Console.WriteLine(prompt);
+	string? input = Console.ReadLine();
+	if (string.IsNullOrEmpty(input))
+	{
+		Console.WriteLine("Input cannot be empty. Please try again.");
+		return promptFor(prompt);
+	}
+	return input;
+}
+
+Name name = new(promptFor("Enter your first name:"), promptFor("Enter your last name:"));
+
+JObject json = new()
+{
+	["Name"] = JObject.FromObject(name)
+};
+
+Console.WriteLine(json.ToString());
+
+
+record Name(string First, string Last);
